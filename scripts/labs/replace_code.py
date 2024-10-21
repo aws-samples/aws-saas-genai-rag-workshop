aa48e93f4a50a640a5a1cf71f2d3de029663cf8c
Ujwal Bukka <ujwbukka@amazon.com>
2024-10-21 17:55:22 -0400

    Added automated copy paste scripts for Lab2

--- scripts/labs/replace_code.py
+++ scripts/labs/replace_code.py
@@ -1,3 +1,5 @@
+import re
+
 def replace_code_in_file(file_path, old_string, new_string):
     try:
         # Open the file for reading and writing
@@ -11,6 +13,26 @@ def replace_code_in_file(file_path, old_string, new_string):
         with open(file_path, 'w') as file:
             file.write(content)
         print(f"Successfully replaced code.")
+        
+    except FileNotFoundError:
+        print(f"The file {file_path} was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def replace_code_in_file_regex(file_path, old_pattern, new_string):
+    try:
+        # Open the file for reading
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+        # Use regex to search for knowledge_base_id with any value
+        content = re.sub(old_pattern, new_string, content, flags=re.DOTALL)
+
+        # Write the modified content back to the file
+        with open(file_path, 'w') as file:
+            file.write(content)
+        
+        print("Successfully replaced code.")
         
     except FileNotFoundError:
         print(f"The file {file_path} was not found.")
